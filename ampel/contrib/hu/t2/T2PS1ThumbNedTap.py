f1ce043d184ab8ba0929fa69f7cdf2344b428c86
Jakob van Santen <jvansanten@gmail.com>
2024-12-05 19:21:39 +0100

    copy check_ned_res from hu-astro

--- ampel/contrib/hu/t2/T2PS1ThumbNedTap.py
+++ ampel/contrib/hu/t2/T2PS1ThumbNedTap.py
@@ -13,11 +13,11 @@ from typing import Literal
 from ampel.abstract.AbsTiedPointT2Unit import AbsTiedPointT2Unit
 from ampel.content.DataPoint import DataPoint
 from ampel.contrib.hu.t2.T2PanStarrThumbPrint import T2PanStarrThumbPrint
-from ampel.contrib.hu.util.ned import check_ned_res
 from ampel.enum.DocumentCode import DocumentCode
 from ampel.model.PlotProperties import FormatModel, PlotProperties
 from ampel.model.UnitModel import UnitModel
 from ampel.plot.create import create_plot_record
+from ampel.protocol.LoggerProtocol import LoggerProtocol
 from ampel.struct.UnitResult import UnitResult
 from ampel.types import UBson
 from ampel.view.T2DocView import T2DocView
@@ -115,3 +115,28 @@ class T2PS1ThumbNedTap(AbsTiedPointT2Unit):
                 break
 
         return {"data": {"plots": plots}}
+
+
+def check_ned_res(
+    cat_res: dict,
+    logger: LoggerProtocol,
+    spectroscopic: bool = False,
+    z_range: None | tuple[float, float] = None,
+) -> bool:
+    if not cat_res.get("z"):
+        logger.info("No redshift found in NED result")
+        return True
+
+    if (
+        spectroscopic
+        and cat_res.get("n_spectra", 0) == 0
+        and cat_res["zflag"] != "SPEC"
+    ):
+        logger.info("Not a spectroscopic redshift")
+        return True
+
+    if z_range and (cat_res["z"] < z_range[0] or cat_res["z"] > z_range[1]):
+        logger.info("Redshift exceeds allowed values")
+        return True
+
+    return False
